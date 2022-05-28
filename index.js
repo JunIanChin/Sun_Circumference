44edb69eebe560bd13217f257abec18820123bcf
Chin Jun Ian <junianchin1226@gmail.com>
2022-05-28 14:31:43 +0800

    Folder structuring

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const aws = require('aws-sdk');
-const pi_handler = require('./pi_algorithm');
-const unitTesting = require('./unitTest');
-require('dotenv').config();
+const {HTTP_CODE, HTTP_MESSAGE, BUCKET_INFO} = require('./utils/constants');
+const reset_pi_handler = require('./utils/resetPi');
 
-/*Local testing of s3 connection */
-aws.config.update({
-    region: 'ap-southeast-1', 
-    apiVersion: 'latest', 
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY, 
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-    }   
-});
+const S3_PARAMS = {
+    Bucket: BUCKET_INFO.BUCKET, 
+    Key: BUCKET_INFO.KEY
+};
+
+exports.handler = async (event) => {
+    console.log(event);
+
+    // Only accept http method 'GET' , invoke through HTTP request
+    if (event.requestContext.http.method !== 'GET') {
+        const bad_method_resp = {
+            statusCode: HTTP_CODE.BAD_REQUEST,
+            body: HTTP_MESSAGE.BAD_REQUEST,
+        }
+
+        return bad_method_resp;
+    }
+
+    try {
+        const s3_connector = new aws.S3(); 
+        const getResetPiValues = reset_pi_handler.precision_default_value(); 
+        const resetPiParams = {
+            ...S3_PARAMS,
+            Body: JSON.stringify(getResetPiValues),
+        };
+
+        await s3_connector.putObject(resetPiParams).promise(); 
+
+        const successResp = {
+            statusCode: HTTP_CODE.OK,
+            body: HTTP_CODE.OK,
+        };
+
+        return successResp;
+    }
+    catch(err) {
+        const server_error_resp = {
+            statusCode: HTTP_CODE.INTERNAL_SERVER_ERROR,
+            body: err.toString('utf-8'),
+        };
+
+        return server_error_resp;   
+    }
+};
 
 (async function main(){
     const s3_query_params = {
---
+++ utils/resetPi.js
@@ -0,0 +1,21 @@
+const precision_default_value = () => {
+    return new Promise (async (resolve, reject) => {
+        try {
+            const default_params = {
+                q : '60',
+                r : '13440',
+                t : '10080' ,
+                i : '3' , 
+                pi: '3'
+            };
+            return resolve(default_params);
+        }
+        catch(err) {
+            return reject(err);
+        }
+    });
+}
+
+module.exports = {
+    precision_default_value,
+}
