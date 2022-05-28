5ea35e393370a3ecbba70dcb445117304b3a8cab
Chin Jun Ian <junianchin1226@gmail.com>
2022-05-28 17:32:07 +0800

    Refactor code to standardize variable naming

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const aws = require('aws-sdk');
 const { HTTP_CODE, HTTP_MESSAGE, BUCKET_INFO, SUN_INFO } = require('./utils/constants');
-const pi_get_next_precision_handler = require('./services/pi_algorithm');
+const nextPiHandler = require('./services/pi_algorithm');
 
 const S3_PARAMS  = {
     Bucket: BUCKET_INFO.BUCKET,
@@ -19,26 +19,26 @@ exports.handler = async (event) => {
     }
 
     try {
-        const s3_connector = new aws.S3(); 
-        const getCurrentPiData = await s3_connector.getObject(S3_PARAMS).promise(); 
-        const get_next_pi_precision = await pi_get_next_precision_handler.get_next_precision(JSON.parse(getCurrentPiData.Body.toString('utf-8')));
+        const s3Connector = new aws.S3(); 
+        const getCurrentPiData = await s3Connector.getObject(S3_PARAMS).promise(); 
+        const newPiData = await nextPiHandler.getNextPrecision(JSON.parse(getCurrentPiData.Body.toString('utf-8')));
 
         const updateParams = {
             ...S3_PARAMS,
-            Body: JSON.stringify(get_next_pi_precision),
+            Body: JSON.stringify(newPiData),
         };
         
-        await s3_connector.putObject(updateParams).promise();
+        await s3Connector.putObject(updateParams).promise();
 
         /* Return latest pi precision value and circumference of sun based on this value*/
-        const decimalify_pi_value = (get_next_pi_precision.pi.slice(0,1).concat('.')).concat(get_next_pi_precision.pi.slice(1,));
-        const latest_pi_precision = parseFloat(decimalify_pi_value);
-        const sun_circumference = latest_pi_precision * parseFloat(SUN_INFO.DIAMETER); 
+        const decimalifiedPi = (newPiData.pi.slice(0,1).concat('.')).concat(newPiData.pi.slice(1,));
+        const latestPiValue = parseFloat(decimalifiedPi);
+        const sunCircumference = latestPiValue * parseFloat(SUN_INFO.DIAMETER); 
         
-        console.log(latest_pi_precision, sun_circumference);
+        console.log(latestPiValue, sunCircumference);
         const queryResponse = {
-            pi: decimalify_pi_value,
-            circumeference: sun_circumference.toPrecision(50),
+            pi: decimalifiedPi,
+            circumference: sunCircumference.toPrecision(50),
         };
 
         const response = {
