c57cc8af477af5580578e0fd53065147d64edf6a
Chin Jun Ian <junianchin1226@gmail.com>
2022-05-30 10:29:44 +0800

    Removed method checking

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
-const aws = require('aws-sdk');
-const {HTTP_CODE, HTTP_MESSAGE, BUCKET_INFO} = require('./utils/constants');
-const resetPiHandler = require('./utils/resetPi');
+const aws = require('aws-sdk')
+const { HTTP_CODE, HTTP_MESSAGE, BUCKET_INFO } = require('./utils/constants')
+const resetPiHandler = require('./utils/resetPi')
 
 const S3_PARAMS = {
-    Bucket: BUCKET_INFO.BUCKET, 
-    Key: BUCKET_INFO.KEY
-};
+  Bucket: BUCKET_INFO.BUCKET,
+  Key: BUCKET_INFO.KEY,
+}
 
 exports.handler = async (event) => {
-    console.log(event);
-
-    // Only accept http method 'GET' , invoke through HTTP request
-    if (event.requestContext.http.method !== 'GET') {
-        const bad_method_resp = {
-            statusCode: HTTP_CODE.BAD_REQUEST,
-            body: HTTP_MESSAGE.BAD_REQUEST,
-        }
-
-        return bad_method_resp;
+  console.log(event)
+  try {
+    const s3Connector = new aws.S3()
+    const getResetPiValues = await resetPiHandler.getDefaultPiValue()
+    const resetPiParams = {
+      ...S3_PARAMS,
+      Body: JSON.stringify(getResetPiValues),
     }
 
-    try {
-        const s3Connector = new aws.S3(); 
-        const getResetPiValues = await resetPiHandler.getDefaultPiValue(); 
-        const resetPiParams = {
-            ...S3_PARAMS,
-            Body: JSON.stringify(getResetPiValues),
-        };
+    await s3Connector.putObject(resetPiParams).promise()
 
-        await s3Connector.putObject(resetPiParams).promise(); 
-
-        const successResp = {
-            statusCode: HTTP_CODE.OK,
-            body: HTTP_MESSAGE.OK,
-        };
-
-        return successResp;
+    const successResp = {
+      statusCode: HTTP_CODE.OK,
+      body: HTTP_MESSAGE.OK,
     }
-    catch(err) {
-        const server_error_resp = {
-            statusCode: HTTP_CODE.INTERNAL_SERVER_ERROR,
-            body: err.toString('utf-8'),
-        };
 
-        return server_error_resp;   
+    return successResp
+  } catch (err) {
+    const server_error_resp = {
+      statusCode: HTTP_CODE.INTERNAL_SERVER_ERROR,
+      body: err.toString('utf-8'),
     }
-};
+
+    return server_error_resp
+  }
+}
